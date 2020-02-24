35764ae565462e65886d26aad9f7bf28963c1547
stojadin2701 <nemanja.stojoski@epfl.ch>
2020-02-24 20:25:56 +0100

    upload main board skeleton code

---
+++ main_board/main_board_rev1_template/Core/Src/tasks/task_template.c
@@ -0,0 +1,20 @@
+/*
+ * task_template.c
+ *
+ *  Created on: Feb 24, 2020
+ *      Author: stoja
+ */
+
+#include "util.h"
+
+#include "tasks/task_template.h"
+
+
+void vTaskTemplate(void *argument) {
+	uint32_t out_int = 1;
+	char* out_str = "template";
+	for (;;) {
+		UsbPrint("[DBG] Hello from %s task %d!\n", out_str, out_int);
+		osDelay(100);
+	}
+}
