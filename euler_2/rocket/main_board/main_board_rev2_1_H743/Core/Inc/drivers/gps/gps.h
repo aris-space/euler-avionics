f5d2ba6287e26831f4f2ef6c6854193c60e3eaca
Luca <lucaj.jost@gmail.com>
2020-10-03 13:47:55 +0200

    Added new gps to mainboard

--- euler_2/rocket/main_board/main_board_rev2_1_H743/Core/Inc/drivers/gps/gps.h
+++ euler_2/rocket/main_board/main_board_rev2_1_H743/Core/Inc/drivers/gps/gps.h
@@ -8,14 +8,10 @@
 #include <util/util.h>
 #include "stm32h7xx_hal.h"
 
-#define HEADER_SIZE 3
-#define BUFFER_SIZE 1000
-#define DATA_SIZE 70
+#define BUFFER_SIZE 18
 #define GPS_NUMBER 3
 
 uint8_t gps_data[GPS_NUMBER][BUFFER_SIZE];
-
-uint8_t gps_gga[GPS_NUMBER][DATA_SIZE];
 
 // *** Macros *** //
 
@@ -26,14 +22,15 @@ uint8_t gps_gga[GPS_NUMBER][DATA_SIZE];
 typedef struct gps_device {
   uint8_t id;
   UART_HandleTypeDef *uart_bus;
+  uint8_t timeout_counter;
   gps_data_t data;
 } UBLOX;
 
 // *** Global Functions *** //
 
-extern uint8_t gps_read_sensor(struct gps_device *dev);
-extern void gps_dma_init(struct gps_device *dev);
-extern void gps_reset(struct gps_device *dev);
+uint8_t gps_read_sensor(struct gps_device *dev);
+void gps_dma_init(struct gps_device *dev);
+void gps_reset(struct gps_device *dev);
 
 extern UART_HandleTypeDef huart1;
 extern UART_HandleTypeDef huart2;
--- euler_2/rocket/main_board/main_board_rev2_1_H743/Core/Src/drivers/gps/gps.c
+++ euler_2/rocket/main_board/main_board_rev2_1_H743/Core/Src/drivers/gps/gps.c
@@ -1,71 +1,60 @@
 #include "drivers/gps/gps.h"
 #include <string.h>
 
-const uint8_t ubxgga[HEADER_SIZE] = {'G', 'G', 'A'};
+uint8_t _parse_data(struct gps_device *dev, uint8_t *data) {
+	uint8_t crc = 0;
+	for(int i = 0; i < 17; i++){
+		crc += data[i];
+	}
+	if(crc == data[17]){
+		dev->data.hour = data[0];
+		dev->data.minute = data[1];
+		dev->data.second = data[2];
 
-uint32_t _gps_search_start(uint8_t *arr) {
-  for (int i = 3; i < BUFFER_SIZE - DATA_SIZE; i++) {
-    if (arr[i - 2] == ubxgga[0] && arr[i - 1] == ubxgga[1] &&
-        arr[i] == ubxgga[2])
-      return i + 1;
-  }
-  return 0;
+		dev->data.lat_deg = data[3];
+		dev->data.lat_decimal = ((uint32_t)data[4]<<24) + ((uint32_t)data[5]<<16) + ((uint32_t)data[6]<<8) + data[7];
+
+		dev->data.lon_deg = data[8];
+		dev->data.lon_decimal = ((uint32_t)data[9]<<24) + ((uint32_t)data[10]<<16) + ((uint32_t)data[11]<<8) + data[12];
+
+		dev->data.fix = data[13];
+
+		dev->data.satellite = data[14];
+
+		dev->data.altitude = ((uint16_t)data[15]<<8) + data [16];
+		return 1;
+	}
+	else return 0;
 }
 
-uint8_t _parse_data(struct gps_device *dev, uint8_t *data) {
-  if (data[22] == 'N' || data[22] == 'S') {
-    // ASCII to decimal conversion
-    for (int i = 0; i < DATA_SIZE; i++) data[i] = data[i] - 48;
-
-    dev->data.hour = data[1] * 10 + data[2];
-    dev->data.minute = data[3] * 10 + data[4];
-    dev->data.second = data[5] * 10 + data[6];
-
-    dev->data.lat_deg = data[11] * 10 + data[12];
-    dev->data.lat_decimal =
-        (data[13] * 1e7 + data[14] * 1e6 + data[16] * 1e5 + data[17] * 1e4 +
-         data[18] * 1e3 + data[19] * 1e2 + data[20]) /
-        6;
-
-    dev->data.lon_deg = data[25] * 10 + data[26];
-    dev->data.lon_decimal =
-        (data[27] * 1e7 + data[28] * 1e6 + data[30] * 1e5 + data[31] * 1e4 +
-         data[32] * 1e3 + data[33] * 1e2 + data[34]) /
-        6;
-
-    dev->data.fix = data[38];
-
-    dev->data.satellite = data[40] * 10 + data[41];
-
-    dev->data.HDOP = data[43] * 100 + data[45] * 10 + data[46];
-
-    dev->data.altitude = data[48] * 100 + data[49] * 10 + data[50];
-
-  } else {
-    dev->data.fix = 0;
-    return 0;
-  }
-  return 1;
+void _buffer_reset(struct gps_device *dev) {
+	for (int i = 0; i < BUFFER_SIZE; i++){
+		gps_data[dev->id][i] = 0;
+	}
 }
 
 void gps_dma_init(struct gps_device *dev) {
   HAL_UART_DMAStop(dev->uart_bus);
-
   HAL_UART_Receive_DMA(dev->uart_bus, gps_data[dev->id], BUFFER_SIZE);
-
   HAL_UART_DMAResume(dev->uart_bus);
 }
 
 uint8_t gps_read_sensor(struct gps_device *dev) {
-  HAL_UART_DMAPause(dev->uart_bus);
-
-  uint32_t data_start = _gps_search_start(gps_data[dev->id]);
-  if (data_start > 0)
-    memcpy(gps_gga[dev->id], gps_data[dev->id] + data_start, DATA_SIZE);
-
-  HAL_UART_DMAResume(dev->uart_bus);
-
-  return _parse_data(dev, gps_gga[dev->id]);
+  if (gps_data[dev->id][17] != 0){
+	  HAL_UART_DMAStop(dev->uart_bus);
+	  _parse_data(dev,gps_data[dev->id]);
+	  _buffer_reset(dev);
+	  HAL_UART_Receive_DMA(dev->uart_bus, gps_data[dev->id], BUFFER_SIZE);
+  } else {
+	  if (dev->timeout_counter >= 5){
+		  _buffer_reset(dev);
+		  HAL_UART_Receive_DMA(dev->uart_bus, gps_data[dev->id], BUFFER_SIZE);
+		  dev->timeout_counter = 0;
+		  return 0;
+	  }
+	  else dev->timeout_counter++;
+  }
+  return 1;
 }
 
 void gps_reset(struct gps_device *dev) {
